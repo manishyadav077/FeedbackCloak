40c86b2c296ecdb11fd3e7556af38c6224b175eb
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-01-06 18:36:34 +0545

    message api created

---
+++ src/app/api/send-messages/route.ts
@@ -0,0 +1,52 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+import { Message } from "@/model/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  const { username, content } = await request.json();
+
+  try {
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      );
+    }
+    if (user.isAcceptingMessage) {
+      return Response.json(
+        {
+          success: false,
+          message: "User is not accepting the message",
+        },
+        { status: 403 }
+      );
+    }
+    const newMessage = { content, createdAt: new Date() };
+    user.messages.push(newMessage as Message);
+    await user.save();
+
+    return Response.json(
+      {
+        success: true,
+        message: "messages sent successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error adding messages", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
+  }
+}
